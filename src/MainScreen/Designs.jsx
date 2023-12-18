f8e252dbd9becd8ca169b541e00ea6d6b37667c1
lasith herath <lasithherath00@gmail.com>
2023-12-18 22:50:31 +0530

    Log out button functions complete

--- src/MainScreen/Designs.jsx
+++ src/MainScreen/Designs.jsx
@@ -3,6 +3,7 @@ import { signOut } from 'firebase/auth';
 import { auth, db } from '../firebase';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { doc, getDoc } from 'firebase/firestore';
+import { Dropdown } from 'antd';
 const Dashboard = () => {
   const [coupleNames, setCoupleNames] = useState([]);
   const { state } = useLocation();
@@ -29,14 +30,54 @@ const Dashboard = () => {
     };
     getCoupleNames();
   }, []);
+
+  const items = [
+    {
+      label: <h1 onClick={logOut}> Log out</h1>,
+      key: '0',
+    },
+    {
+      label: <a href="https://www.aliyun.com">Account Details</a>,
+      key: '1',
+    },
+    {
+      type: 'divider',
+    },
+    {
+      label: 'Help',
+      key: '3',
+    },
+  ];
   console.log('email', state.email);
   return (
     <div>
-      <h1>Dashboard</h1>
+      {/* top nav bar section with log out part */}
+
+      <div className="flex justify-between items-stretch p-5">
+        {/* couple names */}
+        <div className="flex justify-between font-serif text-[40px] gap-5 text-slate-500 ">
+          <h1>{coupleNames.partnerFirstName}</h1>
+          <h1>&</h1>
+          <h1>{coupleNames.yourFirstName}</h1>
+        </div>
+
+        {/* log out Part */}
+        <div className="mr-6">
+          <Dropdown
+            menu={{
+              items,
+            }}
+            trigger={['click']}
+          >
+            <button> Your Account</button>
+          </Dropdown>
+        </div>
+      </div>
+
       {/* {console.log(state)} */}
-      <h1>{coupleNames.partnerFirstName}</h1>
-      <h1>{coupleNames.yourFirstName}</h1>
-      <button onClick={logOut}> Sign Out</button>
+      {/* <h1>{coupleNames.partnerFirstName}</h1>
+      <h1>{coupleNames.yourFirstName}</h1> */}
+      {/* <button onClick={logOut}> Sign Out</button> */}
     </div>
   );
 };
